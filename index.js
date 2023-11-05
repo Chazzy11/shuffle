7938528a61e4d60819d11a6f77d02068abf84870
Chazzy11 <charlotterogers96@hotmail.com>
2023-11-05 13:51:07 +0000

    code written for array shuffling functionality

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello, World")
+function shuffleArray(array) {
+    // if (!Array.isArray(array)) {
+    //     throw new Error('value entered is not an array. ');
+    //   }
+    for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+    }
+    return array;
+    }
+// let myArray = [3, 6, 8, 1]
+//     console.log(shuffleArray(myArray))
