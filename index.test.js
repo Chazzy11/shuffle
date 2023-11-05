8d1e95a7766779ca75a9ceaa8f0f3ed3a400e648
Chazzy11 <charlotterogers96@hotmail.com>
2023-11-05 17:35:33 +0000

    comment added

--- index.test.js
+++ index.test.js
@@ -9,7 +9,7 @@ test("should throw error if input is not array", function () {
 
 test("order of array has been shuffled", function () {
     const testArray = [1, 2, 3];
-    const copyOfTestArray = JSON.parse(JSON.stringify(testArray));
+    const copyOfTestArray = JSON.parse(JSON.stringify(testArray)); // Making a deep copy of the array, otherwise JS will reference the same array
     const shuffledArray = shuffleArray(testArray);
     console.log(shuffledArray);
     expect(shuffledArray).not.toEqual(copyOfTestArray);
