ddba1680eaffeee9b17783b3b5beb6cc394bf3cd
Chazzy11 <charlotterogers96@hotmail.com>
2023-11-05 16:38:47 +0000

    test for if input is not array

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export function shuffleArray(array) {
-    // if (!Array.isArray(array)) {
-    //     throw new Error('value entered is not an array. ');
-    //   }
+    if (!Array.isArray(array)) {
+        throw new Error('value entered is not an array. ');
+      }
     for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     const temp = array[i];
--- index.test.js
+++ index.test.js
@@ -1,2 +1,9 @@
 import { shuffleArray } from "index.js";
+import { test, expect } from "vitest"; 
+
+test("should throw error if input is not array", function () {
+    expect( () => {
+        shuffleArray("not an array")
+    }).toThrow("value entered is not an array.");
+});
 
